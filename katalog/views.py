35a50ac91aff08f85a57fb39f78e9152508f686f
I Made Urip Subagyo <i.made114@ui.ac.id>
2022-09-14 14:53:18 +0700

    tugas 2

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+        'list_katalog' : data_barang_katalog,
+        'nama': 'I Made Urip Subagyo',
+        'id': '2106752243'
+    }
+    return render(request, "katalog.html", context)
